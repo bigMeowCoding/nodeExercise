2c0999c6c1ae168273d373562dec3eddfcf1fc40
zhouyijun <zhouyijun@ruijie.com.cn>
2020-02-26 21:47:34 +0800

    实现获取配置文件内容然后渲染到html中功能

--- server.js
+++ server.js
@@ -1,11 +1,45 @@
 const http = require('http'), fs = require('fs');
 
+function handleError(err, res) {
+    console.error(err);
+    res.end('net error')
+}
+
+function renderTemplateHtml(templateData, configContent, res) {
+    const templateDataString = templateData.toString();
+    const html = templateDataString.replace('%', () => {
+        return configContent.join('</li><li>')
+    });
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    res.end(html);
+}
+
+function getTemplateHtmlAndRender(err, res, configContent) {
+    fs.readFile('./assets/template.html', (error, templateData) => {
+        if (err) {
+            handleError(err, res);
+        }
+        renderTemplateHtml(templateData, configContent, res);
+    })
+}
+
+function renderConfigContent(res) {
+    fs.readFile('./assets/content.json', ((err, data) => {
+        if (err) {
+            handleError(err, res);
+        }
+        const configContent = JSON.parse(data.toString());
+        getTemplateHtmlAndRender(err, res, configContent);
+    }))
+}
+
 http.createServer(
     (req, res,) => {
-        res.writeHead(200, {
-            'Content-Type':'image/jpeg'
-        });
-        fs.createReadStream('./assets/images/yonger.jpeg').pipe(res)
+        if (req.url === '/') {
+            renderConfigContent(res);
+        }
     }
 ).listen(3000);
 
