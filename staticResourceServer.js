d4cdb5c145f075feea9f56ec91b268aadadf8c91
zhouyijun <zhouyijun@ruijie.com.cn>
2020-02-29 18:29:14 +0800

    模拟静态资源服务器

---
+++ staticResourceServer.js
@@ -0,0 +1,35 @@
+const url = require('url'), http = require('http'),
+    fs = require('fs'),
+    path = require('path');
+
+const root = __dirname;
+
+http.createServer(function (req, res) {
+    const urlParsed = url.parse(req.url);
+    const fileDirPath = path.join(root, urlParsed.pathname);
+    fs.stat(fileDirPath, function (error) {
+        if (error) {
+            if (error.code === 'ENOENT') {
+                res.statusCode = 404;
+                res.end('no file');
+            } else {
+                res.statusCode = 500;
+                res.end('internal server error');
+            }
+        } else {
+            const stream = fs.createReadStream(fileDirPath);
+            stream.on('data', function (chunk) {
+                res.write(chunk);
+            });
+            stream.on('end', function () {
+                res.end();
+            });
+            stream.on('error', function (error) {
+                console.error(error);
+                res.statusCode = 500;
+                res.end('internal server error');
+            });
+        }
+    });
+
+}).listen(3000);
