82b62923537dea195ee9101484f220612d212601
zhouyijun <zhouyijun@ruijie.com.cn>
2020-02-29 15:58:57 +0800

    重定向到google

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-const http = require('http'), fs = require('fs');
+const http = require('http');
 
 http.createServer(
     (req, res,) => {
+        const url = 'http://google.com';
+        const body = 'redirect';
+        res.setHeader('Location', url);
+        res.setHeader('Content-Length', body.length);
+        res.setHeader('Content-Type', 'text/html');
+        res.statusCode = 301;
+        res.end(body);
     }
 ).listen(3000);
 
