8ef93896147af66756c27b5116ff6fcd3dd42f05
zhouyijun <zhouyijun@ruijie.com.cn>
2020-03-01 16:14:00 +0800

    利用文件存储

--- server.js
+++ server.js
@@ -1,8 +1,53 @@
-const http = require('http'), fs = require('fs');
+const fs = require('fs'), path = require('path');
 
-http.createServer(
-    (req, res,) => {
-    }
-).listen(3000);
+const args = process.argv.splice(2);
+const file = path.join(process.cwd(), 'tasks.txt');
+const commad = args.shift();
+const taskDescription = args.join(' ');
 
-console.log('serve open');
+function loadTaskArray(file, cb) {
+    fs.exists(file, function (exists) {
+        let tasks = null;
+        if (exists) {
+            fs.readFile(file, 'utf8', function (error, fileData) {
+                if (error) throw error;
+                const data = fileData.toString();
+                tasks = JSON.parse(data) || [];
+                cb(tasks);
+            });
+        } else {
+            storeTasks(file,[taskDescription]);
+        }
+    });
+}
+
+function listTasks(file) {
+    loadTaskArray(file, function (tasks) {
+        for (const task of tasks) {
+            console.log(task);
+        }
+    });
+}
+
+function storeTasks(file, tasks) {
+    fs.writeFile(file, JSON.stringify(tasks), 'utf8', function (err) {
+        if (err) throw err;
+        console.log('Saved');
+    });
+}
+
+function addTask(file, taskDescription) {
+    loadTaskArray(file, function (tasks) {
+        tasks.push(taskDescription);
+        storeTasks(file, tasks);
+    });
+}
+
+switch (commad) {
+    case 'list':
+        listTasks(file);
+        break;
+    case 'add':
+        addTask(file, taskDescription);
+        break;
+}
