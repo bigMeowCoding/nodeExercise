64c47030179f97271427cf51775bd69d1399538c
zhouyijun <zhouyijun@ruijie.com.cn>
2020-03-29 12:41:38 +0800

    配置认证

--- server.js
+++ server.js
@@ -7,10 +7,41 @@ function logger(req, res, next) {
     next();
 }
 
+function restrict(req, res, next) {
+    let auth = req.headers.authorization;
+    if (!auth) {
+        auth = 'Basic amF5OmZlcnJldA==';
+
+        // return next(new Error('unauthorized'));
+    } else {
+        auth = 'Basic amF5OmZlcnJldA==';
+    }
+    const parts = auth.split(' ');
+    const buffer = new Buffer(parts[1], 'base64');
+    const userNamePassword = buffer.toString().split(':');
+    const [userName, password] = userNamePassword;
+    if (userName === 'jay') {
+        next();
+    } else {
+        next(new Error('出错了'));
+    }
+}
+
+function admin(req, res, next) {
+    switch (req.url) {
+        case '/':
+            res.end('try /users');
+            break;
+        case '/users':
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(['tome', 'jack', 'jay']));
+    }
+}
+
 function hello(req, res) {
     res.setHeader('content-type', 'text/plain');
     res.end('hello world');
 }
 
-app.use(logger).use(hello).listen(3000);
+app.use(logger).use('/admin', restrict).use('/admin', admin).use(hello).listen(3000);
 
